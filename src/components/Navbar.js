b39296d7e6892695bd78701ad147f4a8803465ce
PJPriyanka147 <priyankajatav07@gmail.com>
2024-03-04 22:30:21 +0530

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './../index.css';
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='flex flex-row h-24'>
+    <div className="w-24 h-24 px-7 py-7 bg-violet-100">
+    <i class="fa-solid fa-bars fa-2x"></i>
+    </div>
+    <div className="w-full h-24 bg-violet-100"> 
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
